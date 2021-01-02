6e7cc3948ddc8496ef7d8d6855e18e972daafbd2
manuelernesto <manuelernesto@outlook.com>
2021-01-02 08:34:04 +0100

    add updated carToBeUpdated info at CarSerice

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -7,6 +7,7 @@ import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -72,6 +73,8 @@ public class CarService {
                     .map(carToBeUpdated -> {
                         carToBeUpdated.setDetails(car.getDetails());
                         carToBeUpdated.setLocation(car.getLocation());
+                        carToBeUpdated.setCondition(car.getCondition());
+                        carToBeUpdated.setModifiedAt(LocalDateTime.now());
                         return repository.save(carToBeUpdated);
                     }).orElseThrow(CarNotFoundException::new);
         }
